c34472e1b3d1726ded567f35be8a11286150503e
DaryaKorolevskaya <31251742+DaryaKorolevskaya@users.noreply.github.com>
2021-07-03 22:32:44 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -14,12 +14,6 @@ int main(int argc, char *argv[])
     QCoreApplication::setAttribute(Qt::AA_DontUseNativeMenuBar);
     MainWindow w;
     w.show();
-
-
-
-    //QCalendarWidget *calendar = new QCalendarWidget;
-    //calendar->setGridVisible(true);
-    //calendar->setDateRange(QDate(2020, 1, 1), QDate(2021, 1, 1));
-    //calendar->show();
+    
     return a.exec();
 }
