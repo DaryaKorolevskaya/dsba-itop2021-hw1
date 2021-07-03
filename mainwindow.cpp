5010e500f17fa83551c7d392f9a376dcee29c1c3
DaryaKorolevskaya <31251742+DaryaKorolevskaya@users.noreply.github.com>
2021-07-03 22:29:48 +0300

    Update mainwindow.cpp

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -1,16 +1,11 @@
 #include "mainwindow.h"
 #include "./ui_mainwindow.h"
-#include "covidData.h"
-#include "computingData.h"
 #include <QFileDialog>
 #include <QMessageBox>
+#include "model.h"
 
 
-std::vector<covidData> covid;
-std::set<std::string> countries;
-std::string date_from, date_to, country;
-bool infected, died;
-std::pair<std::map<std::string, int>, std::map<std::string, int>> res;
+
 
 
 
@@ -19,7 +14,6 @@ MainWindow::MainWindow(QWidget *parent)
     , ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
-
 
     QPixmap pix(":/img/img/covdata.png");
     int w = ui->image->width();
@@ -39,6 +33,7 @@ MainWindow::MainWindow(QWidget *parent)
 MainWindow::~MainWindow()
 {
     delete ui;
+    delete myModel;
 }
 
 
@@ -48,7 +43,7 @@ void MainWindow::on_infected_clicked()
 {
     if(ui->infected->isChecked())
     {
-        infected = 1;
+        info.infected = 1;
     }
 }
 
@@ -57,51 +52,58 @@ void MainWindow::on_died_clicked()
 {
     if(ui->died->isChecked())
     {
-        died = 1;
+        info.died = 1;
     }
 }
 
 void MainWindow::on_calendarWidget_1_clicked(const QDate &date)
 {
     QDate d1;
-    d1 = ui->calendarWidget_1->selectedDate();
+    d1 = date;
     QString qstr = d1.toString();
-    date_from = qstr.toStdString();
-    //std::cout << date_from;
+    info.date_from = qstr.toStdString();
+
 
 }
 
 void MainWindow::on_calendarWidget_2_clicked(const QDate &date)
 {
     QDate d2;
-    d2 = ui->calendarWidget_2->selectedDate();
+    d2 = date;
     QString qstr2 = d2.toString();
-    date_to = qstr2.toStdString();
-    //std::cout << date_to;
+    info.date_to = qstr2.toStdString();
+
 }
 
 void MainWindow::on_comboBox_currentIndexChanged(const QString &arg1)
 {
-    QString cntr = ui->comboBox->currentText();
-    country = cntr.toStdString();
-    //std::cout<<country;
+
+    QString cntr = arg1;
+    myModel->sel_country = arg1;
+    info.selected_country = cntr.toStdString();
+
+
+
+
+
+
 }
 
 void MainWindow::on_pushButton_clicked()
 {
+    info.result = covidData::computing(info.covid, info.date_from, info.date_to, info.selected_country, info.infected, info.died);
     if (ui->died->isChecked() && ui->infected->isChecked()){
 
-        res = computing(covid, date_from, date_to, country, infected, died);
         QBarSet *set0 = new QBarSet("Died");
         QBarSet *set1 = new QBarSet("Infected");
         int max_i = 0;
 
-        for(auto it = res.second.begin(); it != res.second.end(); ++it)
+        for(auto it = info.result.second.begin(); it != info.result.second.end(); ++it)
           {
             *set0 << it->second;
           }
 
-        for(auto it = res.first.begin(); it != res.first.end(); ++it)
+        for(auto it = info.result.first.begin(); it != info.result.first.end(); ++it)
           {
             if (it->second > max_i)
                 max_i = it->second;
@@ -117,7 +119,7 @@ void MainWindow::on_pushButton_clicked()
          chart->setAnimationOptions(QChart::SeriesAnimations);
 
          QStringList categories;
-         for(auto it = res.first.begin(); it != res.first.end(); ++it)
+         for(auto it = info.result.first.begin(); it != info.result.first.end(); ++it)
            {
                categories << QString::fromStdString(it->first);
            }
@@ -138,16 +140,17 @@ void MainWindow::on_pushButton_clicked()
          QChartView *chartView = new QChartView(chart);
          chartView->setRenderHint(QPainter::Antialiasing);
 
+
          ui->graphicsView->setViewport(chartView);
 
     }
     else {
         if (ui->died->isChecked()){
-            res = computing(covid, date_from, date_to, country, infected, died);
+
             QBarSet *set0 = new QBarSet("Died");
             int max_d = 0;
 
-            for(auto it = res.second.begin(); it != res.second.end(); ++it)
+            for(auto it = info.result.second.begin(); it != info.result.second.end(); ++it)
               {
                 if (it->second > max_d)
                     max_d = it->second;
@@ -163,7 +166,7 @@ void MainWindow::on_pushButton_clicked()
              chart->setAnimationOptions(QChart::SeriesAnimations);
 
              QStringList categories;
-             for(auto it = res.first.begin(); it != res.first.end(); ++it)
+             for(auto it = info.result.first.begin(); it != info.result.first.end(); ++it)
                {
                    categories << QString::fromStdString(it->first);
                }
@@ -189,11 +192,11 @@ void MainWindow::on_pushButton_clicked()
         }
 
         if (ui->infected->isChecked()){
-            res = computing(covid, date_from, date_to, country, infected, died);
+
             QBarSet *set1 = new QBarSet("Infected");
             int max_i = 0;
 
-            for(auto it = res.first.begin(); it != res.first.end(); ++it)
+            for(auto it = info.result.first.begin(); it != info.result.first.end(); ++it)
               {
                 if (it->second > max_i)
                     max_i = it->second;
@@ -210,7 +213,7 @@ void MainWindow::on_pushButton_clicked()
              chart->setAnimationOptions(QChart::SeriesAnimations);
 
              QStringList categories;
-             for(auto it = res.first.begin(); it != res.first.end(); ++it)
+             for(auto it = info.result.first.begin(); it != info.result.first.end(); ++it)
                {
                    categories << QString::fromStdString(it->first);
                }
@@ -244,16 +247,21 @@ void MainWindow::on_actionLoad_data_from_csv_triggered()
     QString filename = QFileDialog::getOpenFileName(this,
                                                     "C://",
                                                     "CSV file (*.csv)");
-    ui->statusbar->showMessage(filename + "is loaded.");
+    ui->statusbar->showMessage(filename + " is loaded.");
 
     std::string fname = filename.toStdString();
-    fillMap(covid, countries, fname);
+    covidData::fillMap(info.covid, info.countries, fname);
+    std::vector<covidData> covData = info.covid;
     std::set<std::string>::iterator it;
-    //for(it = countries.begin(); it != countries.end(); it++){
-    for(auto &item : countries){
+
+    for(auto &item : info.countries){
         QString str = QString::fromStdString(item);
         ui->comboBox->addItem(str);
     }
+    myModel->loadFrom(info.covid);
+    ui->tableView->setModel(myModel);
+
+
 }
 
 void MainWindow::on_actionAbout_Application_triggered()
@@ -266,3 +274,4 @@ void MainWindow::on_actionAbout_Qt_triggered()
 {
     QMessageBox::aboutQt(this);
 }
+
